e2b6c1557c7ee8b27f3b09c71d399319d3c07da2
Ikbal Ramadan〆ANF <heriesr8199@gmail.com>
2026-07-25 00:24:36 +0700

    Update fetch URL to use a live server

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     try {
         console.log("⏳ Sedang menarik data dari server...");
         
-        const respon = await fetch('data_publik.json');
+        const respon = await fetch('https://adjusted-decor-programmers-mechanism.trycloudflare.com/data_publik.json');
         
         if (!respon.ok) throw new Error("File JSON tidak ditemukan!");
         const dataServer = await respon.json();
